89bdfdab7f08bf7fa7ecdc70b22032c571d78a2d
chaserylle <oneandonlychasey@gmail.com>
2018-05-15 02:49:05 +0800

    navbar

--- Customer/pages/home.php
+++ Customer/pages/home.php
@@ -1,14 +1,48 @@
 <?php
-	require "../php/session.php";
+        
+        require "../../connectTodb.php";
+        require "../php/session.php"
 ?>
 
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html>
-<head>
-	<title>Welcome!</title>
-</head>
-<body>
-	<p><?php echo "$current_session"; ?></p>
-	<a href="../php/logout.php"><input type="button" value="Logout"></a>
-</body>
+    
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="../styles/bootstrap-4.0.0/dist/css/bootstrap.css">
+        <link rel="stylesheet" href="../styles/style.css">
+        <title>Qrent</title>
+    </head>
+    
+    <body>
+        <div class="navigation-bar">
+            <div class="nav-element">
+                <a href="home.php"><img  src="../images/qrent-logo.png"></a>
+            </div>
+            <div id="user" class="nav-element">
+                <a href="profile.php">
+                    <?php
+                    echo "$current_session";
+                    ?>
+                </a>
+                <div class="nav-element" id="logout">
+                    <a href="../php/logout.php"><input class="search-button" type="button" value="Logout" id="logout-button"></a>
+                </div>
+            </div>
+            <div id="search">
+                <form action="/action_page.php">
+                      <input type="text" placeholder="Search.." name="search">
+                      <button type="submit">Submit</button>
+                </form>
+            </div>
+
+        </div>
+    <div class="#sidenav">
+    </div>
+    <div class="content">
+        <php 
+    </div>
+
+    </body>
 </html>
--- Customer/pages/profile.php
+++ Customer/pages/profile.php
@@ -1,5 +1,5 @@
 <?php
-	//require "session.php";
+	require "../php/session.php";
 ?>
 
 
@@ -9,30 +9,36 @@
 	<title>Profile</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="../css/style.css">
+<!--    <link rel="stylesheet" href="../styles/bootstrap-4.0.0/dist/css/bootstrap.css">-->
+    <link rel="stylesheet" href="../styles/style.css">
 </head>
 <body>
-<!--
-	<p>
-        <?php
-        //echo "$current_session"; 
-        ?>
-    </p>
--->
     <div class="navigation-bar">
-        <div class="nav_header">
-            <h1><?php
-                //echo "$current_session";
-            ?>
-                My Profile <!--remove this line-->
-            </h1>
+            <div class="nav-element">
+                <a href="home.php"><img  src="../images/qrent-logo.png"></a>
+            </div>
+            <div id="user" class="nav-element">
+                <a href="profile.php">
+                    <?php
+                    echo "$current_session";
+                    ?>
+                </a>
+                <div class="nav-element" id="logout">
+                    <a href="../php/logout.php"><input class="search-button" type="button" value="Logout" id="logout-button"></a>
+                </div>
+            </div>
+            <div id="search">
+                <form action="/action_page.php">
+                      <input type="text" placeholder="Search.." name="search">
+                      <button type="submit">Submit</button>
+                </form>
+            </div>
+
         </div>
-        <div id="logout">
-            <a href="logout.php"><input type="button" value="Logout"></a>
-        </div>
+    <div class="#sidenav">
     </div>
-        
-    
-    
+    <div class="content">
+        <php 
+    </div>
 </body>
 </html>
