9e58933350fa543b44b348ca7f0b9a975b0e9f83
NavarroNorielJon <2161880@slu.edu.ph>
2018-05-15 01:41:19 +0800

    remove something from signup

--- Customer/index.php
+++ Customer/index.php
@@ -53,7 +53,7 @@
         
         <div class="jumbotron">
                 <form method="post">
-                    <img src="images/qrent-logo.png">
+                    <img src="images/qrent-logo.png"><br><br>
                     <div class="form-group">
                         <input type="text" class="form-control" id="username" name="username" placeholder="Username"><br>
                         <input type="password" class="form-control" id="password"  name="password" placeholder="Password"><br>
