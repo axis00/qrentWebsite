ef0c9594c6470840e90df0725f07e96be3d31f87
chaserylle <oneandonlychasey@gmail.com>
2018-05-16 00:32:27 +0800

    display of products fix ko wait

--- Customer/pages/home.php
+++ Customer/pages/home.php
@@ -39,8 +39,8 @@
     <div class="content">
         <?php 
             require "../../connectToDb.php";
-            $query = "SELECT itemName, itemDescription, itemBrand, itemRentPrice, itemOrigPrice, itemCondition  FROM Item ORDER BY 1";
-        
+            $query = "SELECT itemno, itemName, itemDescription, itemBrand, itemRentPrice, itemOrigPrice, itemCondition  FROM Item ORDER BY 1";
+            
             $result = mysqli_query($conn, $query);
             
             
@@ -49,17 +49,18 @@
             $row = mysqli_fetch_array($result, MYSQLI_ASSOC); 
             
             
-                echo "<table class='content-container'>";
-                echo "<tr>
-                        <th>Item name</th>
-                        <th>Item Description</th>
-                        <th>Brand</th>
-                        <th>Price</th>
-                        <th>Original Price</th>
-                        <th>Condition</th>
-                    </tr>";
+//                echo "<table class='content-container'>";
+//                echo "<tr>
+//                        <th>Item name</th>
+//                        <th>Item Description</th>
+//                        <th>Brand</th>
+//                        <th>Price</th>
+//                        <th>Original Price</th>
+//                        <th>Condition</th>
+//                    </tr>";
         
                 while($row = mysqli_fetch_array($result)){
+                $itemno = $row['itemno'];
                 $name = $row['itemName'];
                 $desc = $row['itemDescription'];
                 $brand = $row['itemBrand'];
@@ -67,30 +68,63 @@
                 $ogprice = $row['itemOrigPrice'];
                 $condition = $row['itemCondition'];
                     
-                echo "<tr>
-                        <td>$name</td>
-                      </tr>
-                      <tr>
-                        <td>$desc</td>
-                      </tr>
-                      <tr>
-                        <td>$brand</td>
-                      </tr>
-                      <tr>
-                        <td>$price</td>
-                      </tr>
-                      <tr>
-                        <td>$ogprice</td>
-                      </tr>
-                      <tr>
-                        <td>$condition</td>
-                    </tr>"; 
-        
-                echo "</table>";
-            }
-            
+                $imgquery = "SELECT imagefile FROM qrent.ItemImage where itemno='$itemno' BY 1";
+                $imageq = mysqli_query($conn, $imgquery);
+                    while($r=mysqli_fetch_assoc($imageq))
+                        {
+                            $imagefile = $r["imagefile"];
+                        } 
+//                echo "<br/>";    
+//                echo "<tr>
+//                        <td>$name</td>
+//                      </tr>
+//                      <tr>
+//                        <td>$desc</td>
+//                      </tr>
+//                      <tr>
+//                        <td>$brand</td>
+//                      </tr>
+//                      <tr>
+//                        <td>$price</td>
+//                      </tr>
+//                      <tr>
+//                        <td>$ogprice</td>
+//                      </tr>
+//                      <tr>
+//                        <td>$condition</td>
+//                    </tr>
+//                    
+//                </table>";
+                    echo '<div class="row">';
+                      echo '<div class="col-sm-6 col-md-4">';
+                        echo '<div class="thumbnail">';
+                          echo"<img src='$imagefile'>";
+                          echo'<div class="caption">';
+                            echo"<h3>$name</h3><br/>";
+                            echo"<p>$desc <br/> <b>$price</b> <br/> $ogprice <br/> $condition</p>";
+                            echo'<p><a href=# class="btn btn-primary" role="button">Button</a> <a href=# class="btn btn-default" role="button">Button</a></p>';
+                          echo "</div>
+                        </div>
+                      </div>
+                    </div>";
+                }
             ?>
+        
+<!--
+            <div class="row">
+              <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                  <img src="..." alt="...">
+                  <div class="caption">
+                    <h3>Thumbnail label</h3>
+                    <p>...</p>
+                    <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                  </div>
+                </div>
+              </div>
+            </div>
         </div>
+-->
 
     </body>
 </html>
