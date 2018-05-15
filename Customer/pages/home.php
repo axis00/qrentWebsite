92a231a540648325d1204857c0cb982fb7d2d122
Kristen <kkdvillegas@gmail.com>
2018-05-16 02:22:25 +0800

    transferred style

--- Customer/pages/home.php
+++ Customer/pages/home.php
@@ -9,7 +9,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="../css/style.css">
+        <link rel="stylesheet" href="../styles/style.css">
         <link rel="stylesheet" href="../styles/bootstrap-4.0.0/dist/css/bootstrap.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
         <title>Qrent</title>
@@ -27,7 +27,7 @@
                 
             </div>
                 <?php 
-            require "../../connectToDb.php";
+            require "../php/connectToDb.php";
             $query = "SELECT itemno, itemName, itemDescription, itemBrand, itemRentPrice, itemOrigPrice, itemCondition  FROM Item ORDER BY 1";
             
             $result = mysqli_query($conn, $query);
