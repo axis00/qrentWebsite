42601e40b43ad047804a5f417b5c92e3bf42b7e8
NavarroNorielJon <2161880@slu.edu.ph>
2018-05-16 15:51:03 +0800

    sample

--- Customer/pages/sample.php
+++ Customer/pages/sample.php
@@ -19,6 +19,34 @@
                 <a class="dropdown-item" href="#">Logout</a>
             </div>
         </div>
-        
     </nav>
+    
+    <table>
+        <tr>
+            <th>Item No</th>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Brand</th>
+            <th>Owner</th>
+            <th>Rentprice</th>
+            <th>Condition</th>
+        </tr>
+        
+        <?php
+            require "../php/connectToDb.php";
+            
+            $sql = "SELECT * FROM Item";
+            $results = mysqli_query($conn, $sql);
+            $row = mysqli_fetch_array($results);
+            if($results-> num_rows > 0){
+                while(mysqli_fetch_assoc($results) > 0){
+                    echo "<tr><td>". $row["itemno"] . "</td><td>". $row["itemName"] . "</td><td>" . $row["itemDescription"] . "</td><td>" . $row["itemBrand"] . "</td><td>" .$row["itemOwner"]. "</td> <td>" .$row["itemRentPrice"]. "</td><td>" .$row["itemCondition"]. "</td>" ;
+                }
+            }
+        ?>
+    </table>
 </body>
+
+<?php
+    require "../php/session.php"
+?>
